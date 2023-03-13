60fe9efa71b8e437d7bccf1b5186150cd658a0cb
Nikita Yaroshenko <yaroshenkonikita4@gmail.com>
2023-03-13 19:17:16 +0300

    add piece

---
+++ src/Piece.cpp
@@ -0,0 +1,28 @@
+
+#include "Piece.h"
+
+Piece::Piece(PieceFigure figure, PieceColor color) noexcept {
+    _piece = figure;
+    _color = color;
+}
+
+uint8_t Piece::operator*() noexcept {
+    return _piece * _color;
+}
+
+Piece &Piece::operator=(Piece &other) noexcept {
+    _piece = other._piece;
+    _color = other._color;
+    return *this;
+}
+
+Piece &Piece::operator=(Piece &&other) noexcept {
+    _piece = std::exchange(other._piece, PieceFigure::VOID);
+    _color = std::exchange(other._color, PieceColor::NONE);
+    return *this;
+}
+
+Piece &Piece::operator=(std::pair<Piece::ChessPiece, Piece::ChessColor> &pair) noexcept {
+    _piece = pair.first;
+    _color = pair.second;
+}
---
+++ src/Piece.h
@@ -0,0 +1,40 @@
+#ifndef OLYMPIAD_TRAJECTORY_OF_THE_FUTURE_22_23_SEMIFINAL_DEVOPS_PIECE_H_
+#define OLYMPIAD_TRAJECTORY_OF_THE_FUTURE_22_23_SEMIFINAL_DEVOPS_PIECE_H_
+
+#include <utility>
+#include <cstdint>
+
+class Piece {
+public:
+    typedef enum ChessPiece {
+        VOID = 0,
+        PAWN = 3,
+        ROOK = 5,
+        BISHOP = 7,
+        KNIGHT = 11,
+        QUEEN = 13,
+        KING = 17
+    } PieceFigure;
+    typedef enum ChessColor {
+        NONE = 0,
+        WHITE = 1,
+        BLACK = 2
+    } PieceColor;
+    Piece() noexcept = default;
+    Piece(PieceFigure figure, PieceColor color) noexcept;
+
+    ~Piece() = default;
+
+    uint8_t operator*() noexcept;
+
+    Piece& operator=(Piece &other) noexcept;
+    Piece& operator=(Piece &&other) noexcept;
+    Piece& operator=(std::pair<PieceFigure, ChessColor> &pair) noexcept;
+
+private:
+    PieceFigure _piece{};
+    ChessColor _color{};
+};
+
+
+#endif //OLYMPIAD_TRAJECTORY_OF_THE_FUTURE_22_23_SEMIFINAL_DEVOPS_PIECE_H_
