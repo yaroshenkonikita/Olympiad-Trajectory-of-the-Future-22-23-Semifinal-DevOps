69159a9d7687adf13c5cb32ed0a41d6adc22f48e
Nikita Yaroshenko <yaroshenkonikita4@gmail.com>
2023-03-14 19:03:24 +0300

    debug

--- src/test.cpp
+++ src/test.cpp
@@ -28,8 +28,8 @@ int main(int argc, char **argv) {
 
             std::ostringstream string_query("");
             string_query << "SELECT MAX(MOVE_ID), PIECE_ID, PIECE_COLOR, XAXISMARKER, YAXISNUMBER FROM TEST_VALUES "
-                            "WHERE GAME_ID = " << (i + '0') << " GROUP BY GAME_ID, 2, 3, 4, 5";
-
+                            "WHERE GAME_ID = " << i << " GROUP BY GAME_ID, 2, 3, 4, 5";
+            std::cout << string_query.str() << std::endl;
             pqxx::result response = worker.exec(string_query.str());
             if (!response.size()) {
                 if (i == 1) {
