48b60eacb97ebf8f5b92553f57b8bbe9a60507d3
Никита Ярошенко <yaroshenkonikita4@gmail.com>
2023-03-15 16:53:13 +0300

    add comments & reg pipeline

--- src/Chess.h
+++ src/Chess.h
@@ -7,29 +7,88 @@
 
 class Chess {
  public:
-  static constexpr uint8_t ChessFieldSize = 8;
-  enum ChessFieldMarker { A, B, C, D, E, F, G, H };
-
+  /**
+   * @brief Construct a new Chess object
+   *
+   */
   Chess();
+  /**
+   * @brief Construct a new Chess object with argument
+   *
+   * @param piece_for_fill all field fill that piece
+   */
   Chess(Piece &piece_for_fill);
+  /**
+   * @brief Destroy the Chess object
+   *
+   */
   ~Chess() = default;
 
+  /**
+   * @brief Get the Piece object from Private field
+   *
+   * @param positionNumber a number from 0 to 7 that represents the position of
+   * the digit
+   * @param positionMarker a number from 0 to 7 that represents the position of
+   * the letter
+   * @return Piece& reference on position in field
+   */
   Piece &GetPiece(uint8_t positionNumber, uint8_t positionMarker);
-  int UnderAttackOnDiagonally(Piece::PieceColor ColorKing, int xAxisPos,
+  /**
+   * @brief Check all Diagonally and find attack from another piece
+   *
+   * @param ColorPiece Color of piece
+   * @param xAxisPos Position on digit
+   * @param yAxisPos Position on letter
+   * @return int Who take attack on piece
+   */
+  int UnderAttackOnDiagonally(Piece::PieceColor ColorPiece, int xAxisPos,
                               int yAxisPos);
-  int UnderAttackOnHorizontally(Piece::PieceColor ColorKing, int xAxisPos,
+  /**
+   * @brief Check all Horizontally and find attack from another piece
+   *
+   * @param ColorPiece Color of piece
+   * @param xAxisPos Position on digit
+   * @param yAxisPos Position on letter
+   * @return int Who take attack on piece
+   */
+  int UnderAttackOnHorizontally(Piece::PieceColor ColorPiece, int xAxisPos,
                                 int yAxisPos);
+  /**
+   * @brief Find attack from another piece
+   *
+   * @param positionMarker Position on letter
+   * @param positionNumber Position on digit
+   * @return int Who take attack on piece
+   */
   int IsUnderAttack(uint8_t positionMarker, uint8_t positionNumber);
 
+  /**
+   * @brief Enum for Solution
+   *
+   */
   typedef enum CheckWhiteKing {
     NoCheck,
     CheckFromBishop,
     CheckFromRook
   } SolutionCheck;
+  /**
+   * @brief Solution function on find white king
+   *
+   * @return std::pair<uint8_t, uint8_t> position white king on private field
+   */
   std::pair<uint8_t, uint8_t> FindWhiteKing();
+  /**
+   * @brief Solution - Find check for white king from black bishop or knight
+   *
+   * @return SolutionCheck Result
+   */
   SolutionCheck Solution();
 
  private:
+  static constexpr uint8_t ChessFieldSize = 8;
+  enum ChessFieldMarker { A, B, C, D, E, F, G, H };
+
   Piece ChessField[ChessFieldSize][ChessFieldSize]{};
 };
 
--- src/Piece.h
+++ src/Piece.h
@@ -5,8 +5,17 @@
 #include <stdexcept>
 #include <utility>
 
+/**
+ * @brief
+ *
+ */
 class Piece {
  public:
+  /**
+   * @brief Enum prime numbers so that can determine by division what kind of
+   * figure
+   *
+   */
   typedef enum ChessPiece {
     VOID = 0,
     PAWN = 3,
@@ -16,15 +25,51 @@ class Piece {
     QUEEN = 13,
     KING = 17
   } PieceFigure;
-  typedef enum ChessColor { NONE = 0, WHITE = 1, BLACK = 2 } PieceColor;
-  Piece() noexcept = default;
-  Piece(PieceFigure figure, PieceColor color) noexcept;
 
+  /**
+   * @brief Enum color of piece
+   *
+   */
+  typedef enum ChessColor { NONE = 0, WHITE = 1, BLACK = 2 } PieceColor;
+
+  /**
+   * @brief Construct a new Piece object
+   *
+   */
+  Piece() noexcept = default;
+  /**
+   * @brief Construct a new Piece object with arguments
+   *
+   * @param figure type PieceFigure
+   * @param color type PieceColor
+   */
+  Piece(PieceFigure figure, PieceColor color) noexcept;
+  /**
+   * @brief Destroy the Piece object
+   *
+   */
   ~Piece() = default;
 
+  /**
+   * @brief Static function to get PieceFigure from int
+   *
+   * @param x int equivalent PieceFigure
+   * @return PieceFigure
+   */
   static PieceFigure FromIntToPieceFigure(int x);
+  /**
+   * @brief Static function to get PieceColor from int
+   *
+   * @param x int equivalent PieceColor
+   * @return PieceColor
+   */
   static PieceColor FromIntToPieceColor(int x);
 
+  /**
+   * @brief Operator dereferencing
+   *
+   * @return uint8_t return mul of private fields piece and color
+   */
   uint8_t operator*() noexcept;
 
   Piece &operator=(Piece &other) noexcept;
@@ -33,8 +78,17 @@ class Piece {
   bool operator==(Piece &other) noexcept;
   bool operator!=(Piece &other) noexcept;
 
+  /**
+   * @brief Get the Piece object
+   *
+   * @return PieceFigure
+   */
   PieceFigure GetPiece() const noexcept;
-
+  /**
+   * @brief Get the Color object
+   *
+   * @return PieceColor
+   */
   PieceColor GetColor() const noexcept;
 
  private:
