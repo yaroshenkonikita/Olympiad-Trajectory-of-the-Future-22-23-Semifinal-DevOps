58e7ae97ccb5e9ec7782d8dd2a71bdd1e6d0bb0c
Nikita Yaroshenko <yaroshenkonikita4@gmail.com>
2023-03-16 23:24:30 +0300

    add output

--- src/Tests/googletests.cpp
+++ src/Tests/googletests.cpp
@@ -260,6 +260,7 @@ TEST(Solution, functional_tests_with_pgsql) {
       std::ostringstream answer_query("");
       answer_query << "SELECT ANSWER FROM RESULT_VALUES WHERE ID = " << i;
       pqxx::result answer_check = worker.exec(answer_query.str());
+      std::cout << test;
       switch (answer) {
         case Chess::NoCheck:
           std::cout << "In Test " << i << " King haven't Check" << std::endl;
